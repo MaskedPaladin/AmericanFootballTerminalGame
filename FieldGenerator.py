6036109262db548e4e0eafd50dd389dda3833914
MaskedPaladin <carjien@gmail.com>
2022-03-31 16:18:20 +0200

    put the field generate into a function

---
+++ FieldGenerator.py
@@ -0,0 +1,33 @@
+from Canvas import *
+
+def generateCanvas(xSize, ySize, grassCharacter, lineCharacter, teamACharacter, teamBCharacter):
+    canvas = Canvas(xSize,ySize)
+    for x in range(xSize):
+        for y in range(ySize):
+            if x in range(int(xSize/10)):
+                canvas.putPoint(x, y, teamACharacter)
+            elif x in range(int(xSize-xSize/10), xSize):
+                canvas.putPoint(x, y, teamBCharacter)
+            if x == 0:
+                canvas.putPoint(x, y, lineCharacter)
+            elif x == xSize-1:
+                canvas.putPoint(x, y, lineCharacter)
+            elif y == 0:
+                canvas.putPoint(x, y, lineCharacter)
+            elif y == ySize-1:
+                canvas.putPoint(x, y, lineCharacter)
+            else:
+                canvas.putPoint(x, y, grassCharacter)
+            if y == int(ySize/4):
+                canvas.putPoint(x, y, lineCharacter)
+            elif y == int((ySize-ySize/4)-1):
+                canvas.putPoint(x, y, lineCharacter) 
+            if x in range(1, int(xSize/10)) and y in range(1, ySize-1):
+                canvas.putPoint(x, y, teamACharacter)
+                if x == int(xSize/10-1):
+                   canvas.putPoint(x, y, lineCharacter)
+            elif x in range(int(xSize-xSize/10), xSize-1) and y in range(1, ySize-1):
+                canvas.putPoint(x, y, teamBCharacter)
+                if x == int(xSize-xSize/10):
+                   canvas.putPoint(x, y, lineCharacter)
+    return canvas
